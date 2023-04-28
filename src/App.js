010ac4e1054564d838134e8765886fd9005ed158
Adoyi Godspower <bug84151@gmail.com>
2023-04-28 10:41:12 +0100

    fetch error fix

--- src/App.js
+++ src/App.js
@@ -6,15 +6,26 @@ import Temperature from "./components/Temperature";
 import Wind from "./components/Wind";
 import Description from "./components/Description";
 import FeelsLike from "./components/FeelsLike";
+
 function App() {
   const [data, setData] = useState({});
+  const [error, setError] = useState(null);
+
   const getWeather = (city) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e312cbe5b4d8a35175556c0217f30bc4&units=imperial`;
 
     fetch(url)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not find weather data for this city.");
+        }
+        return res.json();
+      })
       .then((data) => {
         setData(data);
+      })
+      .catch((err) => {
+        setError(err.message);
       });
   };
   return (
@@ -32,6 +43,7 @@ function App() {
           <Header data={data} />
           <Description data={data} />
         </div>
+        {error && <div>{error}</div>}
         <div>
           <FeelsLike data={data} />
         </div>
